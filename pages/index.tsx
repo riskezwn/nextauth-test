dd2896d8eb30f7d2fab090b53dd96890f5d04a67
Enrique García <enrique.garcia@sendizer.com>
2022-10-04 23:37:36 +0200

    Add Nextauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const session = useSession()
+  console.log(session);
+  
   return (
     <div className={styles.container}>
       <Head>
