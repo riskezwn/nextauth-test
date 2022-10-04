2f186829d60a34e0e20dd5af82d2645ee3f5db3c
Enrique García <enrique.garcia@sendizer.com>
2022-10-05 00:29:16 +0200

    add register and login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,53 @@
-export default function loginPage() {
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { getProviders, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default function LoginPage() {
+  const [providers, setProviders] = useState<any>({});
+
+  useEffect(() => {
+    getProviders().then((prov) => {
+      setProviders(prov);
+    });
+  }, []);
+
+  console.log(providers);
+  
+
   return (
-    <h1>Login</h1>
+    <>
+      <h1>Login</h1>
+      dfdffdff
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button
+            onClick={() => {
+              signIn('google')
+            }}
+          >
+            Sign In with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,26 @@
-export default function registerPage() {
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default function RegisterPage() {
   return (
     <h1>Register</h1>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
